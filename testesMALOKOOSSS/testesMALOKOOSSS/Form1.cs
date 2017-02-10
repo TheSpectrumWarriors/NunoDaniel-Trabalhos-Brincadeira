f011b311de2dc5b8088109d897092cbdd4dc47ea
Joao gomes <joaosantigomes@gmail.com>
2017-02-10 18:16:13 +0000

    quico

--- testesMALOKOOSSS/testesMALOKOOSSS/Form1.cs
+++ testesMALOKOOSSS/testesMALOKOOSSS/Form1.cs
@@ -37,15 +37,12 @@ namespace testesMALOKOOSSS
         }
 
 
-       
+        int cont = 0;
 
         private void buttonAdicionarNomes_Click(object sender, EventArgs e)
         {
-
-            int cont = 1;
-
-
-
+            // Eu queria tipo, escrevia um nome e adiciova e deveria aparecer na lista em baixo o nome que eu escrevi
+            // Depois queria adicionar segundo nome e adcionar, e em baxi paarecerem dois nomes
             string[] nomes = new string[10];
 
             nomes[cont] = textBoxNomes.Text;
