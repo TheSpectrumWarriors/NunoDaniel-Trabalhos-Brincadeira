f887aa723d7b488668b3a77e5a21f059b5de5c88
Joao gomes <joaosantigomes@gmail.com>
2017-02-17 16:47:26 +0000

    Gasolineira v1.02

--- GasolineiraJG/GasolineiraJG/Form1.vb
+++ GasolineiraJG/GasolineiraJG/Form1.vb
@@ -1,19 +1,91 @@
 ﻿Public Class FormGasolineira
 
-    Dim Gas, Gas95, Gas98 As Double
-
+    Dim Gas, Gas95, Gas98, preco, valor As Double
 
 
     Private Sub FormGasolineira_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
 
-        Gas95 = InputBox("Qual o preço da Gasolina 95?")
-        Gas98 = InputBox("Qual o preço da Gasolina 98?")
-        Gas = InputBox("Qual o preço do Gasóleo?")
+        Gas95 = IsNumeric(InputBox("Qual o preço da Gasolina 95?"))
+        Gas98 = IsNumeric(InputBox("Qual o preço da Gasolina 98?"))
+        Gas = IsNumeric(InputBox("Qual o preço do Gasóleo?"))
 
         RadioButtonGas95.Checked = True
 
+
+        TextBoxAbastecimento.Text = "0 L"
+
+
+
+
+
     End Sub
+
+    Private Sub ButtonSair_Click(sender As Object, e As EventArgs) Handles ButtonSair.Click
+
+        Sair()
+
+
+
+    End Sub
+
+
+
+    Private Sub TextBoxAbastecimento_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBoxAbastecimento.KeyDown
+        If e.KeyCode = Keys.Enter Then
+
+            ButtonCalcular.PerformClick()
+
+        End If
+    End Sub
+
+    Private Sub ButtonCalcular_Click(sender As Object, e As EventArgs) Handles ButtonCalcular.Click
+
+
+        If (RadioButtonGas95.Checked = True) Then
+            preco = Gas95
+
+            Calcular()
+
+        ElseIf (RadioButtonGas98.Checked = True) Then
+            preco = Gas98
+
+            Calcular()
+
+        ElseIf (RadioButtonGas.Checked = True) Then
+            preco = Gas
+
+            Calcular()
+
+        End If
+
+
+    End Sub
+
+
+    Private Sub Calcular()
+
+        valor = preco * TextBoxAbastecimento.Text
+
+
+        TextBoxTotal.Text = valor
+
+        TextBoxAbastecimento.Focus()
+
+
+    End Sub
+
+
+    Public Sub Sair()
+
+        If MsgBox("Quer mesmo sair?", vbYesNo + vbQuestion, "Atenção!") = vbYes Then
+            End
+        End If
+
+
+
+    End Sub
+
 
 
 
