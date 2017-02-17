0ca25b4cafccddc2b5ee1dbd76d7adc8c7628629
Joao gomes <joaosantigomes@gmail.com>
2017-02-17 18:46:08 +0000

    Não encontro o erro

---
+++ Quadrantes/Quadrantes/Form1.Designer.vb
@@ -0,0 +1,365 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormQuadrantes
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormQuadrantes))
+        Me.TextBoxX = New System.Windows.Forms.TextBox()
+        Me.TextBoxY = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label2Q = New System.Windows.Forms.Label()
+        Me.Label1Q = New System.Windows.Forms.Label()
+        Me.Label3Q = New System.Windows.Forms.Label()
+        Me.Label4Q = New System.Windows.Forms.Label()
+        Me.ButtonCalcular = New System.Windows.Forms.Button()
+        Me.ButtonReset = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBoxX
+        '
+        Me.TextBoxX.Location = New System.Drawing.Point(29, 69)
+        Me.TextBoxX.Name = "TextBoxX"
+        Me.TextBoxX.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxX.TabIndex = 0
+        '
+        'TextBoxY
+        '
+        Me.TextBoxY.Location = New System.Drawing.Point(29, 118)
+        Me.TextBoxY.Name = "TextBoxY"
+        Me.TextBoxY.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxY.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(17, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "X:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(26, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(17, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Y:"
+        '
+        'Label2Q
+        '
+        Me.Label2Q.AutoSize = True
+        Me.Label2Q.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2Q.Location = New System.Drawing.Point(226, 76)
+        Me.Label2Q.Name = "Label2Q"
+        Me.Label2Q.Size = New System.Drawing.Size(45, 24)
+        Me.Label2Q.TabIndex = 4
+        Me.Label2Q.Text = "2ºQ"
+        '
+        'Label1Q
+        '
+        Me.Label1Q.AutoSize = True
+        Me.Label1Q.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1Q.Location = New System.Drawing.Point(350, 76)
+        Me.Label1Q.Name = "Label1Q"
+        Me.Label1Q.Size = New System.Drawing.Size(45, 24)
+        Me.Label1Q.TabIndex = 5
+        Me.Label1Q.Text = "1ºQ"
+        '
+        'Label3Q
+        '
+        Me.Label3Q.AutoSize = True
+        Me.Label3Q.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3Q.Location = New System.Drawing.Point(226, 162)
+        Me.Label3Q.Name = "Label3Q"
+        Me.Label3Q.Size = New System.Drawing.Size(45, 24)
+        Me.Label3Q.TabIndex = 6
+        Me.Label3Q.Text = "3ºQ"
+        '
+        'Label4Q
+        '
+        Me.Label4Q.AutoSize = True
+        Me.Label4Q.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4Q.Location = New System.Drawing.Point(350, 162)
+        Me.Label4Q.Name = "Label4Q"
+        Me.Label4Q.Size = New System.Drawing.Size(45, 24)
+        Me.Label4Q.TabIndex = 7
+        Me.Label4Q.Text = "4ºQ"
+        '
+        'ButtonCalcular
+        '
+        Me.ButtonCalcular.Location = New System.Drawing.Point(29, 162)
+        Me.ButtonCalcular.Name = "ButtonCalcular"
+        Me.ButtonCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonCalcular.TabIndex = 8
+        Me.ButtonCalcular.Text = "Calcular"
+        Me.ButtonCalcular.UseVisualStyleBackColor = True
+        '
+        'ButtonReset
+        '
+        Me.ButtonReset.Location = New System.Drawing.Point(29, 204)
+        Me.ButtonReset.Name = "ButtonReset"
+        Me.ButtonReset.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonReset.TabIndex = 9
+        Me.ButtonReset.Text = "Reset"
+        Me.ButtonReset.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(194, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(241, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "------------------------------------------------------------------------------"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(310, 213)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(9, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "|"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(310, 178)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(9, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "|"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(310, 204)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(9, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "|"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(310, 191)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(9, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "|"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(310, 147)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(9, 13)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "|"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(310, 160)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(9, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "|"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(310, 134)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(9, 13)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "|"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(310, 169)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(9, 13)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "|"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(310, 55)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(9, 13)
+        Me.Label12.TabIndex = 26
+        Me.Label12.Text = "|"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(310, 68)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(9, 13)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = "|"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(310, 42)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(9, 13)
+        Me.Label14.TabIndex = 24
+        Me.Label14.Text = "|"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(310, 77)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(9, 13)
+        Me.Label15.TabIndex = 23
+        Me.Label15.Text = "|"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(310, 99)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(9, 13)
+        Me.Label16.TabIndex = 22
+        Me.Label16.Text = "|"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(310, 112)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(9, 13)
+        Me.Label17.TabIndex = 21
+        Me.Label17.Text = "|"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(310, 86)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(9, 13)
+        Me.Label18.TabIndex = 20
+        Me.Label18.Text = "|"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(310, 121)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(9, 13)
+        Me.Label19.TabIndex = 19
+        Me.Label19.Text = "|"
+        '
+        'FormQuadrantes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 278)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ButtonReset)
+        Me.Controls.Add(Me.ButtonCalcular)
+        Me.Controls.Add(Me.Label4Q)
+        Me.Controls.Add(Me.Label3Q)
+        Me.Controls.Add(Me.Label1Q)
+        Me.Controls.Add(Me.Label2Q)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxY)
+        Me.Controls.Add(Me.TextBoxX)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label19)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FormQuadrantes"
+        Me.Text = "Quadrantes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxX As TextBox
+    Friend WithEvents TextBoxY As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label2Q As Label
+    Friend WithEvents Label1Q As Label
+    Friend WithEvents Label3Q As Label
+    Friend WithEvents Label4Q As Label
+    Friend WithEvents ButtonCalcular As Button
+    Friend WithEvents ButtonReset As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+End Class
---
+++ Quadrantes/Quadrantes/Form1.vb
@@ -0,0 +1,138 @@
+﻿Public Class FormQuadrantes
+
+    Dim X, Y As Double
+
+    Private Sub FormQuadrantes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Label1Q.Visible = False
+        Label2Q.Visible = False
+        Label3Q.Visible = False
+        Label4Q.Visible = False
+
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+
+        Label1Q.Visible = False
+        Label2Q.Visible = False
+        Label3Q.Visible = False
+        Label4Q.Visible = False
+
+        TextBoxX.ReadOnly = False
+        TextBoxY.ReadOnly = False
+
+        TextBoxX.Text = ""
+        TextBoxY.Text = ""
+    End Sub
+
+    Private Sub ButtonCalcular_Click(sender As Object, e As EventArgs) Handles ButtonCalcular.Click
+
+        X = IsNumeric(TextBoxX.Text)
+        Y = IsNumeric(TextBoxY.Text)
+
+        If ((X > 0) And (Y > 0)) Then '1Q
+
+            Label1Q.Visible = True
+            Label2Q.Visible = False
+            Label3Q.Visible = False
+            Label4Q.Visible = False
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+        ElseIf ((X < 0) And (Y > 0)) Then '2Q
+
+            Label1Q.Visible = False
+            Label2Q.Visible = True
+            Label3Q.Visible = False
+            Label4Q.Visible = False
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+        ElseIf ((X < 0) And (Y < 0)) Then '3Q
+
+            Label1Q.Visible = False
+            Label2Q.Visible = False
+            Label3Q.Visible = True
+            Label4Q.Visible = False
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+        ElseIf ((X > 0) And (Y < 0)) Then '4Q
+
+            Label1Q.Visible = False
+            Label2Q.Visible = False
+            Label3Q.Visible = False
+            Label4Q.Visible = True
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+
+
+
+            '------------------------------
+
+        ElseIf ((X = 0) And (Y = 0)) Then ' 0,0
+
+
+            Label1Q.Visible = True
+            Label2Q.Visible = True
+            Label3Q.Visible = True
+            Label4Q.Visible = True
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+
+
+            '-------------------------
+
+        ElseIf ((X = 0) And (Y > 0)) Then ' oY POSITIVO
+
+            Label1Q.Visible = True
+            Label2Q.Visible = True
+            Label3Q.Visible = False
+            Label4Q.Visible = False
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+        ElseIf ((X = 0) And (Y < 0)) Then ' oY NEGATIVO
+
+            Label1Q.Visible = False
+            Label2Q.Visible = False
+            Label3Q.Visible = True
+            Label4Q.Visible = True
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+        ElseIf ((Y = 0) And (X > 0)) Then ' oX POSITIVO
+
+            Label1Q.Visible = True
+            Label2Q.Visible = False
+            Label3Q.Visible = False
+            Label4Q.Visible = True
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+        ElseIf ((Y = 0) And (X < 0)) Then ' oX NEGATIVO
+
+            Label1Q.Visible = False
+            Label2Q.Visible = True
+            Label3Q.Visible = True
+            Label4Q.Visible = False
+
+            TextBoxX.ReadOnly = True
+            TextBoxY.ReadOnly = True
+
+        End If
+
+    End Sub
+
+
+End Class
